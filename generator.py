311b7a7128e2c09d8564bc924b4861e427a2d010
zzhiyuan59 <zzhiyuan@vt.edu>
2024-12-01 07:49:10 -0500

    add more

--- generator.py
+++ generator.py
@@ -1,6 +1,25 @@
-import random
+import random,json
 
-# Generate a list of 10 random (int, int) pairs
-random_pairs = [(random.randint(1, 100), random.randint(1, 100)) for _ in range(10)]
 
-print(random_pairs)
+def random_data(tuple_size = 2):
+    return tuple(random.randint(1, 100)/random.randint(1, 100) for _ in range(tuple_size))
+
+def random_data_list(list_size = 10):
+    data_set = set()
+    while len(data_set) < list_size:
+        data_set.add(random_data())
+    return list(data_set)
+    
+def generate_C0(data_list):
+    mapped_dict = {index: value for index, value in enumerate(data_list)}
+    print(mapped_dict)
+
+    json_output = json.dumps(mapped_dict, indent=4)
+    print(json_output)
+
+    # Save to a file (optional)
+    with open("./C0.json", "w") as json_file:
+        print("Hit!")
+        json_file.write(json_output)
+
+generate_C0(random_data_list())
